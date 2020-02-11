a4c103453326cb60480d44932632ebfa399e823f
Josh Dabuet <josiah.dabuet@gmail.com>
2020-02-11 18:10:35 +0800

    Added FadeinUp effect

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 import staffSlider from './modules/staff-slider';
+import fadeinUpOnScroll from './modules/fadein-up-on-scroll';
 
 staffSlider();
+fadeinUpOnScroll();
---
+++ src/js/modules/fadein-up-on-scroll.js
@@ -0,0 +1,30 @@
+import $ from 'jquery';
+
+export default function fadeinUpOnScroll() {
+    const BREAKPOINT_MOBILE = 768;
+    // fade-in-up on scroll
+    $(window).on('load, scroll', () => {
+        $('.js-fadein').each((i, e) => {
+        	let target = e, offset = 0;
+
+            if($(window).width() > BREAKPOINT_MOBILE){
+            	if($(target).data('offset') != undefined) {
+            		offset = $(target).data('offset');
+            	}
+            	else {
+            		offset = 200;
+            	}
+            }
+            else {
+                offset = 150;
+            }
+
+            let top_of_object = $(target).offset().top + offset;
+            let bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            if( bottom_of_window > top_of_object ){
+                $(target).addClass('is-active');
+            }
+        }); 
+    });
+}
