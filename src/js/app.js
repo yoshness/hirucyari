ae38898d9abb9b56d942798eb72497292985b0c9
Josh Dabuet <josiah.dabuet@gmail.com>
2020-02-15 13:11:49 +0800

    Added footer, menu links and scrollToElement function

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,10 @@ import heroAnimation from './modules/hero-animation';
 import staffSlider from './modules/staff-slider';
 import fadeinUpOnScroll from './modules/fadein-up-on-scroll';
 import toggleMenu from './modules/toggle-menu';
+import scrollToElement from './modules/scroll-to-element';
 
 heroAnimation();
 staffSlider();
 fadeinUpOnScroll();
 toggleMenu();
+scrollToElement();
---
+++ src/js/modules/scroll-to-element.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+
+export default function scrollToElement() {
+    const $trigger = $('.js-scroll');
+
+    $trigger.on('click', (e) => {
+    	e.preventDefault();
+
+    	// if menu item is clicked, close menu first
+    	if($(e.currentTarget).closest('li').hasClass('header__menu-item')) {
+    		$('#js-toggle-menu').click();
+    	}
+
+    	let $target = $($(e.currentTarget).attr('href'));
+        if($target != '') {
+        	$('html, body').stop().animate({ scrollTop: $target.offset().top }, 1000);
+        }
+    });
+}
