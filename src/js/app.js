7e64bfcea3118c3209b6274bfb27cf50aa958afa
Josh Dabuet <josiah.dabuet@gmail.com>
2020-10-13 22:50:53 +0800

    Changed images and CSS/line-break fixes

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,10 @@
 import heroAnimation from './modules/hero-animation';
-import staffSlider from './modules/staff-slider';
 import fadeinUpOnScroll from './modules/fadein-up-on-scroll';
 import toggleMenu from './modules/toggle-menu';
 import scrollToElement from './modules/scroll-to-element';
 import readMore from './modules/read-more';
 
 heroAnimation();
-staffSlider();
 fadeinUpOnScroll();
 toggleMenu();
 scrollToElement();
